1da813bc55202cec5e658ba36ec3e5d69d9106e5
Amin Shazrin <aminshazrin@yahoo.com>
2017-08-19 16:13:52 +0800

    Saja nak push

--- index.js
+++ index.js
@@ -139,17 +139,16 @@ while (i < 0) // Output = Frodo
 
 // Exercises
 function count(x) {
-  var numbers = [];
-  if (x % 2 == 0) { // TODO check that x is even
-    for (var i = 0; i <10; i = i +2) { // TODO assign i to 0; give i the limit of x; and increment by 2
-      numbers.push(i);
+    var numbers = [];
+    if (x % 2 == 0) { // TODO check that x is even
+        for (var i = 0; i < 10; i = i + 2) { // TODO assign i to 0; give i the limit of x; and increment by 2
+            numbers.push(i);
+        }
+    } else {
+        for (var i = 1; i < 10; i = i + 2) { // TODO assign i to 1; give i the limit of x; and increment by 2
+            numbers.push(i);
+        }
     }
-  } else {
-    for (var i = 1; i<10; i = i +2) { // TODO assign i to 1; give i the limit of x; and increment by 2
-      numbers.push(i);
-    }
-  }
-  return numbers;
+    return numbers;
 }
-    console.log(x);
-    
+console.log(x);
